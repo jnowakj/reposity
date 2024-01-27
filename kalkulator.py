9b50d96c767a466179b0c9bf165e11fc0f22bef9
joanna <joanna@debian-joanna.home>
2024-01-27 04:48:05 +0100

    Dodano kalkulator w języku Python.

---
+++ kalkulator.py
@@ -0,0 +1,23 @@
+def dodawanie(x, y):
+	return x + y
+
+def odejmowanie(x, y):
+	return x - y
+
+def mnozenie(x, y):
+	return x * y
+
+def dzielenie(x, y):
+	if y ! 0;
+		return  x / y
+	else:
+		return "Nie można dzielić przez 0."
+
+liczba1 = float(input("Podaj pierwsza liczbę:"))
+liczba2 = float(input("Podaj drugą liczbę:"))
+
+println("Wynik działania:")
+println("Dodawanie:" dodawanie(liczba1,liczba2))
+println("Odejmowanie:" odejmowanie(liczba1, liczba2))
+println("Mnożenie:" mnozenie(liczba1,liczb2))
+println("Dzielenie:" dzielenie(liczba1,liczba2))
